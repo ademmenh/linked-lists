d89a9d9eb42913aab4bb9cf43f694eb8968c665d
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-09-10 23:16:03 +0100

    new definition
    struct intNode, struct intList

---
+++ intLIst/intList.c
@@ -0,0 +1,38 @@
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+typedef struct intNode
+{
+    int Value;
+    struct intNode *Next;
+} intNode;
+
+typedef struct intList
+{
+    intNode *H;
+    int length;
+} intList;
+
+
+
+// 
+
+
+
+
+
+int main ()
+{
+
+    // 
+
+
+
+    return 0;
+}

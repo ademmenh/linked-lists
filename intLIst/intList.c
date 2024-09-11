79721d40ee2c342e6761331f416d744596a02e69
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-09-11 22:00:45 +0100

    new function
    funcintNodePointer ()

--- intLIst/intList.c
+++ intLIst/intList.c
@@ -33,7 +33,7 @@ intNode* funcintNodeCreate (int value)
     intNode *vpNew = (intNode*) malloc (sizeof(intNode));
     if ( vpNew == NULL )
     {
-        print ("the allocation has failled !\n");
+        printf ("the allocation has failled !\n");
         exit (1);
     }
 
@@ -41,6 +41,46 @@ intNode* funcintNodeCreate (int value)
     vpNew->Value = value;
 
     return vpNew;
+}
+
+intNode* funcintNodePointer (intList List, int index)
+{
+
+    intNode *vpCn;
+    int viCn;
+
+    if ( index<0 )
+    {
+        if ( List.length<-index )
+        {
+            printf ("the index is out the range of the List !");
+            exit (1);
+        }
+
+        vpCn = List.H;
+
+        for ( viCn=0; viCn<index; viCn++ )
+        {
+            vpCn = vpCn->Next;
+        }
+        return vpCn->Value;
+    }
+    else
+    {
+        if ( List.length-1<index )
+        {
+            printf ("The index is out the range of the List !");
+            exit (1);
+        }
+
+        vpCn = List.H;
+
+        for ( viCn=0; viCn<index; viCn++ )
+        {
+            vpCn = vpCn->Next;
+        }
+        return vpCn->Value;
+    }
 }
 
 

24a4e48bb223322f2fd1d014a4ffff935b607d97
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-09-17 17:40:20 +0100

    new functions
    funcfloatNodeInit ()
    funcfloatNodeCreate ()

--- floatList/floatList.c
+++ floatList/floatList.c
@@ -16,3 +16,25 @@ typedef struct floatList
     int length;
 } floatList;
 
+
+void funcflaotNodeInit (floatNode *pNode)
+{
+    pNode->Value = 0.0;
+    pNode->Next = NULL;
+}
+
+floatNode* funcfloatNodeCreate (float value)
+{
+
+    floatNode *vpNew = (floatNode*) malloc(sizeof(floatNode));
+    if ( vpNew == NULL )
+    {
+        printf ("the allocation has failled !\n");
+        exit (1);
+    }
+
+    funcfloatNodeInit (vpNew);
+    vpNew->Value = value;
+
+    return vpNew;
+}

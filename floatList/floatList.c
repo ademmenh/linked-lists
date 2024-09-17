6f0a5e92031a7d98a3347e38cb83f69a81e874c6
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-09-17 17:42:45 +0100

    new functions
    funcfloatNodePointer ()
    funcfloatNodePointerBefore ()

--- floatList/floatList.c
+++ floatList/floatList.c
@@ -38,3 +38,62 @@ floatNode* funcfloatNodeCreate (float value)
 
     return vpNew;
 }
+
+floatNode* funcfloatNodePointer (floatList List, int index)
+{
+    if ( index<0 )
+    {
+        printf ("this function do not supports negative indexing !");
+        exit (1);
+    }
+
+    if ( List.length-1<index )
+    {
+        printf ("the index is out the scope !");
+        exit (1);
+    }
+
+    floatNode *vpCn = List.H;
+    int viCn;
+
+    for ( viCn=0; viCn<index; viCn++ )
+    {
+        vpCn = vpCn->Next;
+    }
+    
+    return vpCn;
+}
+
+floatNode* funcfloatNodePointerBefore (floatList List, int index)
+{
+
+    floatNode *vpCn;
+    int viCn;
+
+
+
+    if ( index<0 )
+    {
+        printf ("this function do not supports negative indexing !");
+        exit (1);
+    }
+    
+
+
+    if ( index==0 || List.length<index )
+    {
+        printf ("The index is out the range of the List !");
+        exit (1);
+    }
+
+
+    vpCn = List.H;
+    for ( viCn=0; viCn<index-1; viCn++ )
+    {
+        vpCn = vpCn->Next;
+    }
+    
+    return vpCn;
+
+}
+

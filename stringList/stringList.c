361bd9b574be118b7b68b61b24bda7e377259761
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-09-23 00:36:02 +0100

    new functions
    funcstringNodePointer ()
    funcstringNodePointerBefore ()

--- stringList/stringList.c
+++ stringList/stringList.c
@@ -59,6 +59,64 @@ stringNode* funcstringNodeCreate (string value)
     return vpNew;
 }
 
+stringNode* funcstringNodePointer (stringList List, int index)
+{
+    if ( index<0 )
+    {
+        printf ("this function do not supports negative indexing !");
+        exit (1);
+    }
+
+    if ( List.length-1<index )
+    {
+        printf ("the index is out the scope !");
+        exit (1);
+    }
+
+    stringNode *vpCn = List.H;
+    int viCn;
+
+    for ( viCn=0; viCn<index; viCn++ )
+    {
+        vpCn = vpCn->Next;
+    }
+    
+    return vpCn;
+}
+
+stringNode* funcstringNodePointerBefore (stringList List, int index)
+{
+
+    stringNode *vpCn;
+    int viCn;
+
+
+
+    if ( index<0 )
+    {
+        printf ("this function do not supports negative indexing !");
+        exit (1);
+    }
+    
+
+
+    if ( index==0 || List.length<index )
+    {
+        printf ("The index is out the range of the List !");
+        exit (1);
+    }
+
+
+    vpCn = List.H;
+    for ( viCn=0; viCn<index-1; viCn++ )
+    {
+        vpCn = vpCn->Next;
+    }
+    
+    return vpCn;
+
+}
+
 void funcstringNodeFree (stringNode *pNode)
 {
     funcstringDListClear (&(pNode->Value));
